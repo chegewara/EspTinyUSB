98dd6b76a43fdac642843cb97e0b71a8d9c05f3f
chegewara <imperiaonline4@gmail.com>
2021-02-20 21:27:58 +0100

    Add HID composite with example

---
+++ examples/hid/composite/composite.ino
@@ -0,0 +1,38 @@
+/**
+ * Simple HID mouse and keyboard composite
+ * author: chegewara
+ */
+#include "Arduino.h"
+#include "hidcomposite.h"
+
+HIDcomposite device;
+
+void setup()
+{
+    Serial.begin(115200);
+    device.begin();
+}
+
+void loop()
+{
+    delay(1000);
+    device.doublePressLeft();
+    delay(1000);
+    device.pressRight();
+    delay(1000);
+    device.move(-150, -150);
+    delay(1000);
+    device.move(150, 150);
+    delay(1000);
+    device.pressLeft();
+    delay(1000);
+    device.scrollUp(1);
+    delay(1000);
+    device.scrollDown(1);
+    delay(1000);
+    device.sendKey(HID_KEY_A);
+    delay(1000);
+    Serial.println(device.sendString(String("123456789\n"))?"OK":"FAIL");
+    delay(1000);
+    Serial.println(device.sendString(String("abcdefghijklmnopqrst Uvwxyz\n"))?"OK":"FAIL");    
+}
